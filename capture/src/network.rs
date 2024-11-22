d3bfbbbf9d80d9d0a390012c330bfb5f2da37946
anthony <anthonybruno63@yahoo.com>
2024-11-21 19:26:27 -0800

    trying to encode a utf-8 string in python load it in a packet then capture th epacket in rust to view
    wip

--- capture/src/network.rs
+++ capture/src/network.rs
@@ -1,22 +1,26 @@
 extern crate pnet;
 
 use pnet::datalink::{self, NetworkInterface};
-use pnet::packet::{Packet, ethernet::EthernetPacket};
+use pnet::packet::{ethernet::EthernetPacket, Packet};
 use pnet:: datalink::Channel::Ethernet;
+
+
+
+pub fn get_interfaces() -> Vec<NetworkInterface> {
+    let interfaces = datalink::interfaces();
+    for interface in &interfaces {
+        println!("{:#?}", interface.name);
+    }
+    
+    return interfaces;
+}
+
 
 pub fn network() {
 
-    // print interfaces
-    let interfaces = datalink::interfaces();
-    // for interface in interfaces {
-    //     // println!("{:#?}", interface.name);
-    // }
+    let interfaces = get_interfaces();  
 
-    // select interface you want "lo"
-   
-
-    let all_ints = interfaces;
-    let interface = all_ints
+    let interface = interfaces
                         .into_iter()
                         .find(|iface| iface.name == "lo")
                         .expect("Failed to find interface");
@@ -32,8 +36,12 @@ pub fn network() {
             Ok(packet) => {
                 let eth_packet = EthernetPacket::new(packet).unwrap();
                 // Now that we can capture the packets we need to send them out.
-                println!("{:#?}", packet);
 
+                let payload = match std::str::from_utf8(eth_packet.payload()){
+                    Ok(v) => println!("{}", v),
+                    Err(e) => println!("Invalid: {}", e),
+                };
+                
             },
             Err(e) => {
                 eprintln!("Failed to receive packet: {:?}", e);
--- testutils/testutils/send_test.py
+++ testutils/testutils/send_test.py
@@ -1,16 +1,15 @@
 from scapy.all import *
-
+import time
 
 
 
 # File to test spamming a network interface with packets to test capturing from rust side
 
 def main():
-    pkt = Ether()/IP(dst="127.0.0.1")/TCP(dport=80)
-
-    sendp(pkt, iface="lo", loop=1, inter=0.2)
-
+    data = "abc".encode('utf-8')
+    pkt = Ether()/IP(dst="127.0.0.1")/TCP(dport=80)/Raw(load=data)
     
+    sendp(pkt, loop=1,inter=1);
     
 
 
