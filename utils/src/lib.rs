02766a35551db087521c8f0f2ea251f8ea7b46f6
anthony <anthonybruno63@yahoo.com>
2024-11-30 17:09:57 -0800

    clean up utils lib

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,14 +1,2 @@
 
 pub mod dictionary;
-use dictionary::Dictionary;
-use std::path::Path;
-
-fn main() {
-
-    let names = Path::new("bin/first-names.txt");
-    let mut dic = Dictionary::new(names);
-    dic.print();
-    let out = dic.random();
-    println!("out: {:?}", out);
-
-}
