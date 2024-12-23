2a7b891e875f5a0d302d3984ef90fce6feaa2c17
anthony <anthonybruno63@yahoo.com>
2024-12-23 11:08:55 -0800

    Able To create and load udp packet.

--- network/src/network_sender.rs
+++ network/src/network_sender.rs
@@ -3,14 +3,20 @@
 use std::path::Path;
 use pnet:: datalink;
 use pnet::packet::ethernet::{EtherTypes, MutableEthernetPacket};
-use pnet::packet::ipv4::{ Ipv4Packet, MutableIpv4Packet};
-use pnet::packet::Packet;
-use pnet::util::MacAddr;
+use pnet::packet::ip::IpNextHeaderProtocols;
+use pnet::packet::ipv4::{ checksum as ipv4_checksum, MutableIpv4Packet};
+use pnet::packet::{Packet, PacketSize};
+use pnet::packet::udp::{ ipv4_checksum as udp_ipv4_checksum, MutableUdpPacket};
+
+
+
 use std::thread::sleep;
 use utils::dictionary::Dictionary;
 use crate::network_capture::get_interfaces;
 use std::time::Duration;
 use std::net::IpAddr;
+
+
 
 
 pub fn send_packets() {
@@ -43,58 +49,64 @@ pub fn send_packets() {
     loop {
         let data = dic.random();
         println!("name: {}", data);
+
+        // Setup checksum
+        let source_ip = match lo_int.ips[0].ip() {
+            IpAddr::V4(ipv4) => {ipv4}
+            IpAddr::V6(_ipv6) => { 
+                eprintln!("Not working with ipv6");
+                0.into()
+            }
+        };
+
+        let destination_ip = source_ip;
         
-        //Build IPv4 Payload
-        let mut ipv4_payload = vec![0u8; 20];
+        //Build Udp Packet
+
+        let mut udp_buffer = vec![0u8; MutableUdpPacket::minimum_packet_size() + data.len()];
         {
-            let mut ipv4_packet = MutableIpv4Packet::new(&mut ipv4_payload).expect("error");
+            let mut udp_packet = MutableUdpPacket::new(&mut udp_buffer).expect("Error");
+            udp_packet.set_source(80);
+            udp_packet.set_destination(80);
+            udp_packet.set_length((MutableUdpPacket::minimum_packet_size() + data.len()) as u16);
+            udp_packet.set_payload(data.as_bytes());
+        
+
+
+            let udp_checksum = udp_ipv4_checksum(&udp_packet.to_immutable(), &source_ip, &destination_ip);
+            udp_packet.set_checksum(udp_checksum);
+        }
+
+        //Build IPv4
+        let mut ipv4_buff = vec![0u8; 20 + udp_buffer.len()];
+        {
+            let mut ipv4_packet = MutableIpv4Packet::new(&mut ipv4_buff).expect("error");
             ipv4_packet.set_version(4);
             ipv4_packet.set_header_length(5);
-            ipv4_packet.set_total_length(20);
+            ipv4_packet.set_total_length((20 + udp_buffer.len()) as u16);
             ipv4_packet.set_identification(0x1234);
             ipv4_packet.set_ttl(64);
-            ipv4_packet.set_next_level_protocol(pnet::packet::ip::IpNextHeaderProtocols::Udp);
-            ipv4_packet.set_source(
-                    match lo_int.ips[0].ip() {
-                        IpAddr::V4(ipv4) => {ipv4}
-                        IpAddr::V6(ipv6) => { 
-                            eprintln!("Not working with ipv6");
-                            0.into()
-                        }
-                    }
-                );
-            ipv4_packet.set_destination(
-                    match lo_int.ips[0].ip() {
-                        IpAddr::V4(ipv4) => {ipv4}
-                        IpAddr::V6(ipv6) => { 
-                            eprintln!("Not working with ipv6");
-                            0.into()
-                        }
-                    }
-                );
-            ipv4_packet.set_checksum(pnet::packet::ipv4::checksum(&ipv4_packet.to_immutable())); 
+            ipv4_packet.set_next_level_protocol(IpNextHeaderProtocols::Udp);
+            ipv4_packet.set_source(source_ip);
+            ipv4_packet.set_destination(destination_ip);
+            ipv4_packet.set_payload(&udp_buffer);
+            ipv4_packet.set_checksum(ipv4_checksum(&ipv4_packet.to_immutable()));
 
         }
 
-
-
-        // Build ethernet frame
-        let mut frame = vec![0u8; MutableEthernetPacket::minimum_packet_size() + ipv4_payload.len()];
+        // // Build ethernet frame
+        let mut frame = vec![0u8; MutableEthernetPacket::minimum_packet_size() + ipv4_buff.len()];
         {
             let mut packet = MutableEthernetPacket::new(&mut frame).unwrap();
             packet.set_destination(lo_int.mac.expect("failed to get mac"));
             packet.set_source(lo_int.mac.expect("failed to get mac"));
             packet.set_ethertype(EtherTypes::Ipv4);
-            packet.set_payload(&ipv4_payload);
+            packet.set_payload(&ipv4_buff);
 
         }
-         
-
-
-
 
         match tx.send_to(&frame, Some(lo_int.clone())) {
-            Some(Ok(_)) => println!("Sent packet!"),
+            Some(Ok(_)) => println!("Sent packet: {:?}", frame),
             Some(Err(e)) => eprintln!("Failed to send packet: {}", e),
             None => eprintln!("Failed to send packet: No result")
         }
