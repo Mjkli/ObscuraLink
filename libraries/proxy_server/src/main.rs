1506207ec33e5354817cad307e29b57467f120ae
anthony <anthonybruno63@yahoo.com>
2025-01-23 19:07:16 -0800

    Proxy server library is setup in a simple way.

--- libraries/proxy_server/src/main.rs
+++ libraries/proxy_server/src/main.rs
@@ -1,7 +1,43 @@
 
-//
-//
+use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+use hyper_util::rt::TokioIo;
 
-fn main() {
-    println!("Hello, world!");
+
+async fn forward(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>,Infallible> {
+    println!("Found request");
+    Ok(Response::new(Full::new(Bytes::from("Hello World"))))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = SocketAddr::from(([127,0,0,1], 3000));
+    
+    let listener = TcpListener::bind(addr).await?;
+
+    // proxy service to catch reuqests
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        let io = TokioIo::new(stream);
+
+        //Spawn a tokio task to server multiple connections
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new()
+                .serve_connection(io, service_fn(forward))
+                .await
+            {
+                eprintln!("Error serving connection: {:?}", err);
+            }
+        });
+
+    }
+
+
 }
